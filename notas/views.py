eb027f79e8417172ee81c0f75b3e6daaad6ec9b0
Byron Corrales <byroncorrales@gmail.com>
2013-04-01 00:25:42 -0600

    css fixes

--- notas/views.py
+++ notas/views.py
@@ -82,8 +82,7 @@ def lista_notas_pais(request,id):
 
 def index(request):
 
-    notas = Notas.objects.all().order_by('-fecha','-id')[:3]
-    notas2 = Notas.objects.all().order_by('-fecha','-id')[3:6]
+    notas = Notas.objects.all().order_by('-fecha','-id')[:2]
     evento = Agendas.objects.filter(publico=True).order_by('-inicio')[:3]
     paises = Pais.objects.all()
     contrapartes = Contraparte.objects.all()
