71da9dda1f7434206cacefc92dc1afb2bb9e46a1
sacrac <crocha09.09@gmail.com>
2013-05-03 15:05:15 -0600

    fix orden

--- contrapartes/models.py
+++ contrapartes/models.py
@@ -66,7 +66,7 @@ class Contraparte(models.Model):
     class Meta:
         verbose_name_plural = "Asociados"
         unique_together = ("font_color", "nombre")
-        ordering = ['frecuecia',]
+        ordering = ['-frecuecia',]
 
     def __unicode__(self):
         return self.nombre
